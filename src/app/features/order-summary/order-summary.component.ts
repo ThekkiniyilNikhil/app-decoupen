98d13e89ae00a00908b41f52a5a7668d1befa0b4
ThekkiniyilNikhil <nikhil.kandoth@gmail.com>
2025-03-05 23:07:00 +0530

    looged in user with summary ui done

--- src/app/features/order-summary/order-summary.component.ts
+++ src/app/features/order-summary/order-summary.component.ts
@@ -2,14 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgxOtpInputComponent, NgxOtpInputComponentOptions } from 'ngx-otp-input';
+import { Checkbox } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-order-summary',
-  imports: [CommonModule, FormsModule, NgxOtpInputComponent],
+  imports: [CommonModule, FormsModule, NgxOtpInputComponent, Checkbox],
   templateUrl: './order-summary.component.html',
   styleUrl: './order-summary.component.scss'
 })
 export class OrderSummaryComponent {
+
+  useDecouponBalance: boolean = true;
+  guestFullName: string;
+  guestMobNumber: string;
+  guestEmailId: string;
+  isEnterGSTDetails: boolean = false;
+  gstRegNumber: string;
+  gstCompanyName: string;
+  gstCompanyAddress: string;
+
   otpOptions: NgxOtpInputComponentOptions = {
     otpLength: 6,
     autoFocus: true,
